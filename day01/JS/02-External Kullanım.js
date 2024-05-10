1a4d49c0ae02fae5fc581d45962e80983b7935e8
abdullahcaliskan2561 <152506826+abdullahcaliskan2561@users.noreply.github.com>
2024-05-11 00:09:53 +0300

    v10May

---
+++ day01/JS/02-External Kullanım.js
@@ -0,0 +1,17 @@
+console.log('Bu bir external kullanımdır');
+/* Consola gittiğimizde bu çıktıyı görebileceğiz */
+
+/* //Javascript kodları harici bir dokümana konularak kullanılırsa buna external script denir  ve bu dosyayı kullanan tüm html sayfalarında kullanılabilir hale gelir*/
+
+
+//Console türleri
+
+console.log('Hello JS');
+
+console.warn('Bu bir uyarıdır');//Consolda başında sarı  uyarı üçgeni olarak görünür
+
+console.error('Bu bir hatadır');  //Consolda başında kırmızı çarpı işareti olarak gelir
+
+console.info('Bu bir bilgi mesajıdır');//Bilgi vermek amaçlı kullanılır
+
+
