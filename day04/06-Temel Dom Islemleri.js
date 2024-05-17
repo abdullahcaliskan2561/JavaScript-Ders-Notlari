0c9251662f6f5446edd0f6660603961df82827f8
abdullahcaliskan2561 <152506826+abdullahcaliskan2561@users.noreply.github.com>
2024-05-18 00:40:09 +0300

    add

--- day04/06-Temel Dom Islemleri.js
+++ day04/06-Temel Dom Islemleri.js
@@ -1,5 +1,21 @@
 //1
 function setMode() {
     const isDark = document.querySelector("body").classList.contains("dark");
-    console.log(isDark);
+    //console.log(isDark); Body nin classlistinde dark varsa true verir yoksa false verir
+
+    //3
+   const btnToggle= document.querySelector(".toggle-btn");
+
+
+    //2
+    if (isDark) {
+        document.body.classList.remove("dark");
+        btnToggle.innerHTML =' <i class="fas fa-moon"></i>'  //4
+
+    }
+    else {
+        document.body.classList.add("dark");
+        btnToggle.innerHTML ='<i class="fas fa-sun"></i>'  //5
+    }
+
 }
