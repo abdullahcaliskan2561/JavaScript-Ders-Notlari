a1a3002fce1c701093e2cbc7290f68dd64cdfdfb
abdullahcaliskan2561 <152506826+abdullahcaliskan2561@users.noreply.github.com>
2024-06-29 16:24:02 +0300

    June29

--- day13/02.Dom.js
+++ day13/02.Dom.js
@@ -1,22 +1,41 @@
 import { students } from "./02.Dom-Data.js";
 
-//Students i html deki tbody içerisine ekleyen bir fonksiyon yazalım
+//Butona tıklama event ekleyelim-3
+document.getElementById("btnShowLowScores").addEventListener("click", () => {
+    setRowColorsForLowScores();   
+});
+
+//Satırları gezen ve renk değişimi yazan kod yazalım
+const setRowColorsForLowScores = () => {
+    const tableRows = document.querySelectorAll("#tblStudents tbody tr");
+
+    tableRows.forEach((tr) => {
+        const score = tr.querySelector("td:last-child").textContent;
+        if (score < 80) {
+            tr.classList.add("table-danger");
+        }
+    });
+};
+
+//Students i html deki tbody içerisine kleyen bir fonksiyon yazalım-1
 const loadData = () => {
     let html = "";
-    students.forEach((student ,index) => {
+    students.forEach((student, index) => {
         html += `<tr>
             <td>${index + 1}</td>
             <td>${student.name}</td>
             <td>${student.point}</td>
         </tr>`;
-    })
-//tbady yi seçelim
+    });
+
+//tbady yi seçelim-2
 const tbody = document.querySelector("#tblStudents tbody");
 tbody.innerHTML = html;
 
 }
-
-
-
 loadData();
 
+
+
+
+ 
