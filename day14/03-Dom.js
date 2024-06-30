a4463eee7d2e17a56de85e2f89329177cb9e4241
abdullahcaliskan2561 <152506826+abdullahcaliskan2561@users.noreply.github.com>
2024-06-30 04:43:44 +0300

    June30

---
+++ day14/03-Dom.js
@@ -0,0 +1,51 @@
+import { students } from "../day13/02.Dom-Data.js";
+
+document.querySelector("#tblStudents tbody").addEventListener("click", (e) => {
+    // tbody icinde uzerine tiklanan her elementin click olayinin burada yakalanmasi eventPropogation sayesinde olur.
+
+    if (e.target.classList.contains("btn-del")) {
+        deleteRow(e.target);
+    } else {
+        toggleRow(e.target);
+    }
+});
+
+const deleteRow = (btn) => {
+    const row = btn.closest("tr");
+    const name = row.children[1].textContent;
+
+    const res = confirm(
+        `${name} isimli kaydi silmek istediginizden emin misiniz?`
+    );
+
+    if (res) {
+        row.remove();
+    }
+};
+
+const toggleRow = (el) => {
+    el.closest("tr").classList.toggle("table-info");
+};
+
+const loadData = () => {
+    let html = "";
+
+    students.forEach((student, index) => {
+        html += `<tr>
+                    <td>${index + 1}</td>
+                    <td>${student.name}</td>
+                    <td>${student.point}</td>
+                    <td>
+                        <button class="btn btn-danger btn-sm btn-del">🗑️</button>
+                    </td>
+                </tr>`;
+    });
+
+    const tbody = document.querySelector("#tblStudents tbody");
+    tbody.innerHTML = html;
+};
+
+loadData();
+
+//Buraya kadar yapmıştık Simdi kaldığımız yerden seçince silme işlemeni yapmasını yapacağız
+//Bu örnek day13 06-DomPractise.html örneğinin devamıdır
---
+++ day14/06-Error Handling.js
@@ -0,0 +1,46 @@
+
+
+
+const frmLogin = document.querySelector("#frmLogin");
+
+frmLogin.addEventListener("submit", (e) => {
+    e.preventDefault();//Formun default submit davranışını iptal eder Default davranışta login e tıklayınca bilgileri google a gönderir Bu şekilde  default davranışı kapatınca login e tıklayınca bilgileri göndermeyecek
+
+    const txtEmail = document.getElementById("txtEmail");
+    const txtPassword = document.getElementById("txtPassword");
+
+
+
+
+
+
+    try {
+        const email = txtEmail.value;
+        const password = txtPassword.value;
+
+        if (!email || !isEmail(email)) {
+            txtEmail.focus();
+            throw new Error("Lütfen email adresinizi  giriniz");
+            
+        }
+
+        if (!password) {
+            txtPassword.focus();
+            throw new Error("Lütfen sifrenizi giriniz");
+        }
+
+        //Yukarıda kapattığımız default submit özelliğini tekrar açalım Tıkladığımızda google a gönderecek
+        e.target.submit();
+
+    } catch (error) {
+        console.log(error);
+        alert(error.message);
+
+    }  
+});
+
+//Regex ile verilen e mail adresinin uygun formatta olup olmadığını kontrol eden bir fonksiyon yazalım 
+const isEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);  
+}
